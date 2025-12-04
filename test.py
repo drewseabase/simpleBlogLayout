ba69c6133ef0cc28a16115a3937f4a5ee7069436
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-03 23:04:07 -0500

    testing out python, getting into it again

---
+++ test.py
@@ -0,0 +1,15 @@
+print("hello world!")
+print("Hello World!")
+a = 1
+b = 2
+c = a + b
+name = "Drew" #string
+age = 21 # integer
+height = 5.9 # float
+print(c)
+print(name)
+print(height)
+print(age + 2)
+
+
+
