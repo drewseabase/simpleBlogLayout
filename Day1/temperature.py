b56bcd30a1f3e31bd406466012b8eb86ca3858f9
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-12 16:38:51 -0500

    quick committ

--- Day1/temperature.py
+++ Day1/temperature.py
@@ -1,53 +1,32 @@
-#Cookie Party Planner
-cookies_baked = 125
-teammates_coming = 18
+#Exercise #1
+number1 = int(input("Enter an integer "))
+number2 = int(input("Enter an integer "))
 
-amount_for = cookies_baked // teammates_coming
-amount_left = cookies_baked % teammates_coming
+calculation = number1 * number2
 
-print(f'There are {amount_for} cookies for all the teammates')
-print(f'There are {amount_left} cookies left')
+if calculation <= 1000:
+    print(f'The result is {calculation}')
+else:
+    calculation = number1 + number2
+    print(f'The result is {calculation}')
 
-#Calculating savings account growth
-principal = 800
-rate = 0.05
-time_growth = 3
+#Exercise #2
+number3 = int(input("Enter an integer "))
 
-final_amount = round(principal * (1 + rate) ** time_growth, 2)
-total_interest = round(final_amount - principal, 2) 
+for i in range(number3):
+    current_num = i
+    previous_num = current_num - 1
+    if previous_num < 0:
+        previous_num = 0
+    sum = current_num + previous_num
+    print(f'Current number {current_num} previous number {previous_num} sum: {sum}')
 
-print(f'After three years youll have {final_amount} in your bank account assuming it is compounded once per year')
-print(f'Total interest earned is {total_interest}')
+#Exercise #3
 
-#Leveling Up in a game
+word = input("Enter a word ")
 
-level_xp = 500
-current_xp = 4327
-
-level_current = current_xp // level_xp
-xp_towards_nextlvl = current_xp % level_xp
-total_xp_needed = level_xp - xp_towards_nextlvl
-print(f'Your current level is {level_current}')
-print(f'You have {xp_towards_nextlvl} xp in the current level')
-print(f'You need {total_xp_needed} xp to reach the next level ')
-
-#Tiling Room
-tiles_sold = 12
-area_of_room = 4 * 5
-area_of_tile = 0.5 * 0.5
-tiles_needed = int(area_of_room / area_of_tile)
-full_boxes_needed = int(tiles_needed // tiles_sold)
-
-print(f'You need {tiles_needed} tiles and {full_boxes_needed} boxes')
-
-#Splitting Posts into pages
-
-total_posts = 137
-posts_per_page = 10
-
-full_pages = total_posts // posts_per_page
-posts_spill_over = total_posts % full_pages
-total_pages = full_pages + 1
-print(f'You have {full_pages} pages with {posts_spill_over} posts on the last one')
-print(f'Your total amount of pages are {total_pages}')
+print(f'Original String is {word}')
+print(f'Printing only even index chars')
+for i in range(0, len(word), 3):
+    print(word[i])
 
---
+++ newpractice.py
@@ -0,0 +1,6 @@
+name = input("Enter a string ")
+
+print(name[0], name[-1])
+
+print(name[2])
+print(name[-2])
