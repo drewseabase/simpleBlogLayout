52a23ed3457ea7b28629c9e48d4d64318f3d56cb
drewseabase <143631824+drewseabase@users.noreply.github.com>
2026-02-14 11:34:15 -0500

    okie

--- rle.js
+++ rle.js
@@ -1,18 +1,40 @@
 function encode(str){
-   let myMap = new Map();
-   let split = str.split('');
-   
+   let myArr = [];
+   let count = 1;
 
-   for(let item of split){
-    if(myMap.has(item)){
-        myMap.set(item, myMap.get(item) + 1);
+   for(let i = 0; i < str.length; i++){
+    if(str[i] === str[i + 1]){
+        count ++;
+        
     }else{
-        myMap.set(item, 1);
-    } 
+        myArr.push(count > 1 ? `${count}${str[i]}` : `${str[i]}`);
+        count = 1;
+    }
    }
+  
+   return myArr.join('');
+}
+function decode(str){
+    let num = 1;
+    let finalStr = '';
 
-   return myMap;
+    for(let i = 0; i < str.length; i++){
+        if(/^\d/.test(str[i]) && /^\d/.test(str[i+1])){
+            num = Number(str[i] + str[i + 1]);
+            i++
+        }else if( /^\d/.test(str[i])){
+            num = Number(str[i]);
+        }else{
+            finalStr += str[i].repeat(num);
+            num = 1;
+        }
+    }
+
+    return finalStr;
 }
 
-let str = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB';
+
+let str = 'AABBBCCCC';
+let otherStr = '2 hs2q q2w2';
 console.log(encode(str));
+console.log(decode(otherStr));
