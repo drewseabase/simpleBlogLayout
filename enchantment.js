3924c00e1263d716f067ec3f1a52c652f0b01b33
drewseabase <143631824+drewseabase@users.noreply.github.com>
2026-03-05 17:01:39 -0500

    continuation

--- enchantment.js
+++ enchantment.js
@@ -32,4 +32,33 @@ console.log(getSecondCard(deck));
 console.log(swapTwoCards(deck));
 console.log(shiftThreeCardsAround(deck));
 console.log(pickNamedPile(piles));
-console.log(swapNamedPile(piles));
+swapNamedPile(piles);
+
+
+function getCardPosition(stack, card){
+     return stack.findIndex((value) => value === card);
+}
+function doesStackIncludeCard(stack, card){
+    return stack.includes(card);
+}
+function isEachCardEven(stack){
+    return stack.every((value) => value % 2 === 0);
+}
+function doesStackIncludeOddCard(stack){
+    return stack.some((value) => value % 2 !== 0);
+}
+function getFirstOddCard(stack){
+    return stack.find((value) => value % 2 !== 0);
+}
+function getFirstEvenCardPosition(stack){
+    return stack.findIndex((value) => value % 2 === 0);
+}
+
+const stack = [9,7,3,2];
+const card = 2;
+console.log(getCardPosition(stack, card));
+console.log(doesStackIncludeCard(stack,card));
+console.log(isEachCardEven(stack));
+console.log(doesStackIncludeCard(stack));
+console.log(getFirstOddCard(stack));
+console.log(getFirstEvenCardPosition(stack));
